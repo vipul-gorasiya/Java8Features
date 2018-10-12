6b317a863ef49567afd99c904db4253c3d802c6d
vipul-gorasiya <39234040+vipul-gorasiya@users.noreply.github.com>
2018-10-12 10:20:32 +0530

    added comments and more examples

--- src/com/vipul/examples/lambda/FunctionalInterfaceLambdaExample.java
+++ src/com/vipul/examples/lambda/FunctionalInterfaceLambdaExample.java
@@ -13,15 +13,28 @@ public class FunctionalInterfaceLambdaExample {
 
 		// providing lambda as functional interface implementation with
 		// different conditions
+
+		// Print basic Phones
+		System.out.println("Basic Phones: ");
+		printMatchingForms(examples, p -> !(p instanceof SmartPhone));
+		System.out.println();
+
 		// Print smart Phones
 		System.out.println("Smart Phones: ");
 		printMatchingForms(examples, p -> p instanceof SmartPhone);
+		System.out.println();
 
 		// print phone with front cameras
 		System.out.println("Phones with front camera: ");
 		printMatchingForms(examples, p -> p instanceof SmartPhone && ((SmartPhone) p).getFrontCamera().isPresent());
 	}
 
+	/**
+	 * Prints phones which are satisfying criteria provided in searchPhone
+	 * 
+	 * @param examples
+	 * @param searchPhone
+	 */
 	private static void printMatchingForms(LambdaExamples examples, SearchSmartPhone searchPhone) {
 		examples.getPhones().forEach(p -> {
 			if (searchPhone.testPhone(p))
